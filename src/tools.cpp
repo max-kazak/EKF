8b1d9d9b8e254af404bdaf43c384995ccbdc0fe3
Max Kazakov <m@xkazak.com>
2020-06-26 21:22:03 -0600

    +tools.cpp

--- src/tools.cpp
+++ src/tools.cpp
@@ -11,14 +11,58 @@ Tools::~Tools() {}
 
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
-  /**
-   * TODO: Calculate the RMSE here.
-   */
+  VectorXd rmse = VectorXd::Zero(4);
+
+  if(estimations.size() == 0) {
+    std::cout << "ERROR: CalculateRMSE: estimations is empty" << std::endl;
+    return rmse;
+  }
+  if(ground_truth.size() == 0) {
+    std::cout << "ERROR: CalculateRMSE: ground_truth is empty" << std::endl;
+    return rmse;
+  }
+  if(estimations.size() != ground_truth.size()) {
+    std::cout << "ERROR: CalculateRMSE: estimations ground_truth are not the same size" << std::endl;
+    return rmse;
+  }
+
+  for(int i=0; i<estimations.size(); i++){
+    VectorXd diff = estimations[i] - ground_truth[i];
+    diff = diff.array() * diff.array();
+    rmse += diff;
+  }
+
+  rmse = rmse / estimations.size();
+  rmse = rmse.array().sqrt();
+
+  return rmse;
 }
 
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
-  /**
-   * TODO:
-   * Calculate a Jacobian here.
-   */
+  MatrixXd Hj(3,4);
+
+  if(x_state.size() != 4){
+    std::cout << "ERROR: CalculateJacobian: state vector must be of size 4." << std::endl;
+    return Hj;
+  }
+
+  double px = x_state(0);
+  double py = x_state(1);
+  double vx = x_state(2);
+  double vy = x_state(3);
+
+  double c1 = px*px + py*py;
+  double c2 = sqrt(c1);
+  double c3 = c1*c2;
+
+  if(fabs(c1) < 0.0001) {
+    std::cout << "ERROR: CalculateJacobian: potential division by zero." << std::endl;
+    return Hj;
+  }
+
+  Hj << (px/c2),              (py/c2),                0,      0,
+       -(py/c1),              (px/c1),                0,      0,
+       py*(vx*py - vy*px)/c3, px*(px*vy - py*vx)/c3,  px/c2,  py/c2;
+
+  return Hj;
 }
